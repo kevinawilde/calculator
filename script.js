fd94bd2b3ff3ea6e099665760dd71a7a9fdc7189
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-03-28 21:53:47 -0500

    calculator fully functional, now to make it pretty

--- script.js
+++ script.js
@@ -6,50 +6,62 @@ const calculator = {
 
 };
 var result;
+var display = function(numbers) {
+    var display = document.getElementById('display');
+
+    display.textContent = numbers;
+}
+var clearDisplay = function(){
+	var display = document.getElementById('display');
+
+    display.textContent = "0";
+}
 var number = function(elem, calculate) {
 	if(calculate.waiting == false){
 		calculate.firstNumber= calculate.firstNumber + '' + elem;
 		calculate.firstNumber= parseInt(calculate.firstNumber, 10);
+		display(calculate.firstNumber);
 	} else if(calculate.waiting == true) {
 		calculate.secondNumber= calculate.secondNumber + '' + elem;
 		calculate.secondNumber= parseInt(calculate.secondNumber, 10);
+		display(calculate.secondNumber);
 	}
 	return calculate;
 };
 var operator = function(operation, calculate){
 	calculate.waiting = true;
 	calculate.operator = operation;
+	clearDisplay();
 	return calculate;
 };
 var calculation= function(calculate){
 	if (calculate.operator == '+'){
 		result = (calculate.firstNumber + calculate.secondNumber);
+		display(result);
 		return result;
 	}
 	if (calculate.operator == '-'){
 		result = (calculate.firstNumber - calculate.secondNumber);
+		display(result);
 		return result;
 	}
 	if (calculate.operator == '*'){
 		result = (calculate.firstNumber * calculate.secondNumber);
+		display(result);
 		return result;
 	}
 	if (calculate.operator == '/'){
 		result = (calculate.firstNumber / calculate.secondNumber);
+		display(result);
 		return result;
 	}
 }
-var clear = function(calculate){
+var clearCalculator = function(calculate){
+	clearDisplay();
 	calculate.firstNumber = 0;
 	calculate.secondNumber = 0;
 	calculate.waiting = false;
 	calculate.operator = null;
+	
 	return calculate;
 }
-number(21, calculator);
-operator('/', calculator);
-number(3 ,calculator);
-calculation(calculator);
-console.log(result);
-clear(calculator);
-console.log(calculator);
